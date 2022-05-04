3b92d522fe2b46539f6ff0c45cb864b8729420d8
Stephano <stephanokparaskeva@gmail.com>
2022-05-04 12:04:22 +0100

    uses local svg rather than font awesome

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,28 +1,20 @@
 import Link from "next/link";
 import classes from "./MainNavigation.module.css";
-import Head from "next/head";
+import Image from "next/image";
 
 function MainNavigation() {
   return (
-    <>
-      <Head>
-        <script
-          src="https://kit.fontawesome.com/1ae37fca73.js"
-          crossOrigin="anonymous"
-        ></script>
-      </Head>
-      <header className={classes.header}>
-        <Link href="/">
-          <div className={classes.logo}>React Meetups</div>
-        </Link>
-        <Link href="/new-meetup">
-          <div className={classes.addMeetupContainer}>
-            <div className={classes.addMeetupText}>Add Meetup</div>{" "}
-            <i className="fa-solid fa-plus"></i>
-          </div>
-        </Link>
-      </header>
-    </>
+    <header className={classes.header}>
+      <Link href="/">
+        <div className={classes.logo}>React Meetups</div>
+      </Link>
+      <Link href="/new-meetup">
+        <div className={classes.addMeetupContainer}>
+          <div className={classes.addMeetupText}>Add Meetup</div>{" "}
+          <Image width={25} height={25} src={"/plus.svg"} />
+        </div>
+      </Link>
+    </header>
   );
 }
 
