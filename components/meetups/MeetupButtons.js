4c274e0164260561def6ed0a8077f6cb6d6b980c
Stephano <stephanokparaskeva@gmail.com>
2022-05-06 12:07:33 +0100

    removes back button

--- components/meetups/MeetupButtons.js
+++ components/meetups/MeetupButtons.js
@@ -1,12 +1,9 @@
 import classes from "./MeetupButtons.module.css";
-import { useRouter } from "next/router";
 
 const MeetupButtons = ({ editing, setEditing, deleteHandler, loading }) => {
-  const router = useRouter();
 
   return (
     <div className={classes.actions}>
-      <button className={classes.back} onClick={router.back}>Back</button>
       <button onClick={deleteHandler}>
         {loading ? "Loading..." : "Delete"}
       </button>
